7a6fb5b124bd934e0281608c47fbbed6c550458e
João Vítor <joaovsoutosantos@gmail.com>
2021-12-23 13:43:57 -0300

    :tada: Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '86e046f2a18c9ce7cd88bc9ec924f160'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5124bce7-ac15-49dd-8294-2dc38c6287e5'
+};
