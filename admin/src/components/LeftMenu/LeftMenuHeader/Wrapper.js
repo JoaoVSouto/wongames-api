add4ff062779eade2d01850961aaaf6ed7c3a7d1
João Vítor <joaovsoutosantos@gmail.com>
2021-12-24 12:47:27 -0300

    change menu logo and its styles

---
+++ admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import Logo from '../../../assets/images/logo-won.svg';
+
+const Wrapper = styled.div`
+  background-color: ${(props) =>
+    props.theme.main.colors.leftMenu['background-header-link']};
+  height: ${props => props.theme.main.sizes.leftMenu.height};
+
+  .projectName {
+    display: block;
+    height: ${props => props.theme.main.sizes.leftMenu.height};
+
+    background-image: url(${Logo});
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: 12rem;
+  }
+`;
+
+Wrapper.defaultProps = {
+  theme: {
+    main: {
+      colors: {
+        leftMenu: {},
+      },
+      sizes: {
+        header: {},
+        leftMenu: {},
+      },
+    },
+  },
+};
+
+Wrapper.propTypes = {
+  theme: PropTypes.object,
+};
+
+export default Wrapper;
