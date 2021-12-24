ee5aa1f27b1cef64bb59e5628b44c40d25b754a7
João Vítor <joaovsoutosantos@gmail.com>
2021-12-24 17:23:55 -0300

    fetch and scrape game info with jsdom

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -2,6 +2,20 @@
 
 const axios = require("axios");
 
+async function getGameInfo(slug) {
+  const { JSDOM } = require("jsdom");
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const description = dom.window.document.querySelector(".description");
+
+  return {
+    rating: "BR0",
+    shortDescription: description.textContent.slice(0, 160),
+    description: description.innerHTML,
+  };
+}
+
 module.exports = {
   async populate() {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -9,5 +23,7 @@ module.exports = {
     const {
       data: { products },
     } = await axios.get(gogApiUrl);
+
+    console.log(await getGameInfo(products[0].slug));
   },
 };
